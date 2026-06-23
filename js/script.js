a0b035d3495a66159194e7dc2f43798980f49d82
DoPhucKhang <khang241205@gmail.com>
2026-06-23 09:25:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// Thông tin website cá nhân
+let tenWebsite = "Website Cá Nhân";
+let chuDe = "Giới thiệu bản thân";
+let soLuongAnh = 15;
+let trangThaiHoanThanh = true;
 
+// In thông tin ra Console
+console.log("Tên website:", tenWebsite);
+console.log("Chủ đề:", chuDe);
+console.log("Số lượng ảnh:", soLuongAnh);
+console.log("Trạng thái hoàn thành:", trangThaiHoanThanh);
